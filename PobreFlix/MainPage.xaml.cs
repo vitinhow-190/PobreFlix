66d36de3e50788e507ff9913c50cd16bae2178ed
Usuario <Usuario@CPD03-M17>
2024-04-29 15:07:32 -0300

    sim

--- PobreFlix/MainPage.xaml.cs
+++ PobreFlix/MainPage.xaml.cs
@@ -2,23 +2,41 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+       
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void b1_Clicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void b3_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b4_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b5_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b6_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b2_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 
