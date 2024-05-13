1b766d5054857b18970230dbc93d3efc25e7797b
Usuario <Usuario@CPD03-M17>
2024-05-13 15:02:29 -0300

    atualizações

--- PobreFlix/MainPage.xaml.cs
+++ PobreFlix/MainPage.xaml.cs
@@ -34,7 +34,37 @@
 
         }
 
+        private void b7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
         private void b2_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b8_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b9_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b10_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b11_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b12_Clicked(object sender, EventArgs e)
         {
 
         }
