4f645d3554dc1244b079b8e8382cddf83452abb4
Usuario <Usuario@CPD03-M17>
2024-06-13 14:21:16 -0300

    atualizações

--- PobreFlix/Categorias/Guerra.xaml.cs
+++ PobreFlix/Categorias/Guerra.xaml.cs
@@ -6,4 +6,24 @@ public partial class Guerra : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void troia_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void SniperAmericano_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void milnovessentos_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Dunkirk_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
